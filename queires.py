b014ef7e0a4a4b1bca36677ba0f4b631f65dcb59
Zsinka Zoltán <zsinka.zoltan@gmail.com>
2021-12-07 21:46:35 +0100

    Register a new user now works properly. Login works properly, sending error message if the user or the password wrong. Session time works, then automaticly clearing the session. Secret app key also added. Minimal design element added to the bootstrap5 Navbar elements depending from the logging.

--- queires.py
+++ queires.py
@@ -1,14 +1,28 @@
 import data_manager
+import json
 
 
-def add_user(input_username, input_firstname, input_lastname, input_email, password_hashed):
-    user = data_manager.execute_select(
-        f"""
-        INSERT INTO users (username, firstname, lastname, email, password_hashed)
-        VALUES ( %(un)S '{input_firstname}, {input_lastname}, {input_email}, {password_hashed}')
-        
-        """
-    )
+def reg_user(input_username, input_email, password_hashed):
+    data_manager.execute_insert(
+        '''
+        INSERT INTO users (username, email, password_hashed)
+        VALUES ( %(un)s, %(em)s, %(psswh)s )
+        ;
+        ''',
+        {'un': input_username, 'em': input_email, 'psswh': password_hashed})
+
+
+def login_user(user_name):
+    username = data_manager.execute_select(
+        '''
+        SELECT * 
+        FROM users
+        WHERE username = %(input_username)s
+        ;
+        ''',
+        {'input_username': user_name})
+
+    return username
 
 
 def get_card_status(status_id):
@@ -22,8 +36,8 @@ def get_card_status(status_id):
         SELECT * FROM statuses s
         WHERE s.id = %(status_id)s
         ;
-        """
-        , {"status_id": status_id})
+        """,
+        {"status_id": status_id})
 
     return status
 
@@ -34,7 +48,7 @@ def get_boards():
     :return:
     """
     # remove this code once you implement the database
-    return [{"title": "board1", "id": 1}, {"title": "board2", "id": 2}]
+    # return [{"title": "board1", "id": 1}, {"title": "board2", "id": 2}]
 
     return data_manager.execute_select(
         """
@@ -46,7 +60,7 @@ def get_boards():
 
 def get_cards_for_board(board_id):
     # remove this code once you implement the database
-    return [{"title": "title1", "id": 1}, {"title": "board2", "id": 2}]
+    # return [{"title": "title1", "id": 1}, {"title": "board2", "id": 2}]
 
     matching_cards = data_manager.execute_select(
         """
