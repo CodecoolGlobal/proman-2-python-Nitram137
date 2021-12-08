83e2e47db1723bfd2aa596b968cffb098f636418
Guszti9 <tusi0531@gmail.com>
2021-12-08 11:22:46 +0100

    make cards appear under status

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -9,26 +9,23 @@ export let boardsManager = {
     const boards = await dataHandler.getBoards();
     const boardBuilder = htmlFactory(htmlTemplates.board);
     const statusBuilder = htmlFactory(htmlTemplates.status);
+    const cardBuilder = htmlFactory(htmlTemplates.card);
 
     for (let board of boards) {
       const content = boardBuilder(board);
-      const statuses = await dataHandler.getStatuses(board.id);
       domManager.addChild("#root", content);
+      const statuses = await dataHandler.getStatuses(board.id);
+      const cards = await dataHandler.getCardsByBoardId(board.id);
       for (let status of statuses) {
         const boardContent = statusBuilder(status);
-        domManager.addChild(`.board[data-board-id="${board.id}"]`, boardContent)
+        domManager.addChild(`.board[data-board-id="${board.id}"]`, boardContent);
+        for (let card of cards) {
+          if (card.status_id === status.id) {
+            const statusContent = cardBuilder(card);
+            domManager.addChild(`.status[data-status-id="${status.id}"]`, statusContent);
+          }
+        }
       }
-      domManager.addEventListener(
-        `.toggle-board-button[data-board-id="${board.id}"]`,
-        "click",
-        showHideButtonHandler
-      );
     }
   }
 };
-
-function showHideButtonHandler(clickEvent) {
-  const boardId = clickEvent.target.dataset.boardId;
-  statusesManager.loadStatuses(boardId)
-  cardsManager.loadCards(boardId);
-}
